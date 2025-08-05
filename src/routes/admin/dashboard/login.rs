57d2e97820b90b2dad6062c52b39bb28103331a9
Milo Weinberg <iapetus011@gmail.com>
2025-08-04 22:47:54 -0400

    Misc improvements + continue login page

--- src/routes/admin/dashboard/login.rs
+++ src/routes/admin/dashboard/login.rs
@@ -1,12 +1,24 @@
-use poem::{get, web::Data};
+use askama::Template;
+use poem::{
+    get, post,
+    web::{Data, Html},
+};
 
 pub fn routes() -> poem::Route {
     poem::Route::new()
-        .at("", get(get_login))
-        .at("", )
+        .at("", get(get_login).post(post_login))
+}
+
+#[derive(askama::Template)]
+#[template(path = "views/admin/dashboard/login.html")]
+struct LoginViewTemplate {}
+
+#[poem::handler]
+pub async fn get_login() -> Html<String> {
+    Html(LoginViewTemplate {}.render().unwrap())
 }
 
 #[poem::handler]
-pub async fn get_login(Data(db_pool): Data<&sqlx::PgPool>) {
+pub async fn post_login(Data(db_pool): Data<&sqlx::PgPool>) {
     todo!();
 }
--- src/routes/admin/dashboard/mod.rs
+++ src/routes/admin/dashboard/mod.rs
@@ -1,10 +1,8 @@
-use poem::get;
-
 mod home;
 mod login;
 
 pub fn routes() -> poem::Route {
     poem::Route::new()
         .nest("", home::routes())
-        .at("/login/", login::routes())
+        .nest("/login/", login::routes())
 }
